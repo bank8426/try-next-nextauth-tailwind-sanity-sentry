2be0c892fbeb85188fd4dc70963dd07a86922d9d
Tanapon Kuljarusin <bank_55555@hotmail.com>
2025-06-13 15:59:58 +0700

    add user detail page, add UserStartup component, add queries AUTHOR_BY_ID_QUERY, STARTUPS_BY_AUTHOR_QUERY

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -1,9 +1,10 @@
-import { formatDate } from "@/lib/utils";
+import { cn, formatDate } from "@/lib/utils";
 import { EyeIcon } from "lucide-react";
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Author, Startup } from "@/sanity/types";
+import { Skeleton } from "./ui/skeleton";
 
 export type StartupCardType = Omit<Startup, "author"> & { author?: Author };
 
@@ -42,7 +43,7 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
         <Link href={`user/${author?._id}`}>
           <Image
             src={author?.image || "https://placehold.co/48x48"}
-            alt="placeholder"
+            alt={author?.name}
             width={48}
             height={48}
             className="rounded-full"
@@ -67,6 +68,16 @@ const StartupCard = ({ post }: { post: StartupCardType }) => {
       </div>
     </li>
   );
+};
+
+export const StartupCardSkeleton = () => {
+  <>
+    {[0, 1, 2, 3, 4].map((i: number) => (
+      <li key={cn("skeletion", i)}>
+        <Skeleton className="startup-card_skeleton" />
+      </li>
+    ))}
+  </>;
 };
 
 export default StartupCard;
